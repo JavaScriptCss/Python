5a39f0430432204dd6197721edbc997ee5d407dd
JavascriptCss <631092124@qq.com>
2018-10-08 18:28:47 +0800

    logos

---
+++ demo1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+# -*- coding:utf-8 -*-
+from turtle import*
+import turtle
+turtle.color("blue")
+x=100
+# speed(10000)
+turtle.speed(1000)
+while x<500:
+    forward(x)
+    left(160)
+    x=x+8
+done( )
